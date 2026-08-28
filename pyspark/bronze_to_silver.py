9b27f0f3400c443d149b727e5d490c1e71137a41
Purvaja Dodke <purvaja510@gmail.com>
2026-08-28 20:43:35 +0530

    feat: add Bronze to Silver transformation

--- pyspark/bronze_to_silver.py
+++ pyspark/bronze_to_silver.py
@@ -1,12 +1,26 @@
 from pyspark.sql import functions as F
 
-# Read marketplace data from the Bronze Delta table
+# Read marketplace data from Bronze
 marketplace_bronze = spark.table(
     "workspace.bronze.marketplace_listings"
 )
 
-# Check the Bronze schema
-marketplace_bronze.printSchema()
+# Bronze -> Silver transformation
+marketplace_silver = (
+    marketplace_bronze
+    .drop("_rescued_data")
+    .withColumn("price", F.col("price").cast("double"))
+    .withColumn("scraped_at", F.to_timestamp("scraped_at"))
+    .dropDuplicates(["listing_url"])
+    .filter(F.col("product_title").isNotNull())
+)
 
-# Preview the Bronze records
-marketplace_bronze.show(20, truncate=False)
+# Preview Silver data
+marketplace_silver.printSchema()
+marketplace_silver.show(20, truncate=False)
+
+# Write Silver Delta table
+marketplace_silver.write \
+    .format("delta") \
+    .mode("overwrite") \
+    .saveAsTable("workspace.silver.marketplace_listings")
