238947f05f4f71ad26ec4f83cc0db0bcdd57beed
Purvaja Dodke <purvaja510@gmail.com>
2026-08-30 17:57:36 +0530

    feat: add product title normalization

---
+++ pyspark/fuzzy_matching.py
@@ -0,0 +1,27 @@
+from pyspark.sql import functions as F
+
+# Read marketplace data from Silver
+marketplace_silver = spark.table(
+    "workspace.silver.marketplace_listings"
+)
+
+# Create a normalized product title
+marketplace_normalized = (
+    marketplace_silver
+    .withColumn(
+        "normalized_title",
+        F.lower(
+            F.regexp_replace(
+                F.col("product_title"),
+                r"[^a-zA-Z0-9 ]",
+                ""
+            )
+        )
+    )
+)
+
+# Preview normalized titles
+marketplace_normalized.select(
+    "product_title",
+    "normalized_title"
+).show(20, truncate=False)
